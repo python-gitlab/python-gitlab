c261875cf167e6858d052dc983fb0dcb03e3ea40
Christian Wenk <christian.wenk@omicronenergy.com>
2016-04-21 13:37:48 +0200

    Remove 'next_url' from kwargs before passing it to the cls constructor.
    The 'next_url' argument causes problems in the _construct_url method if it
    doesn't belong there. E.g. if you list all projects, change an attribute
    of a project and then try to save it, the _construct_url will use the
    'next_url' from the list method and the save will fail.

--- gitlab/__init__.py
+++ gitlab/__init__.py
@@ -281,7 +281,7 @@ class Gitlab(object):
         get_all_results = kwargs.get('all', False)
 
         # Remove parameters from kwargs before passing it to constructor
-        for key in ['all', 'page', 'per_page', 'sudo']:
+        for key in ['all', 'page', 'per_page', 'sudo', 'next_url']:
             if key in cls_kwargs:
                 del cls_kwargs[key]
 
@@ -385,7 +385,7 @@ class Gitlab(object):
         get_all_results = params.get('all', False)
 
         # Remove parameters from kwargs before passing it to constructor
-        for key in ['all', 'page', 'per_page', 'sudo']:
+        for key in ['all', 'page', 'per_page', 'sudo', 'next_url']:
             if key in cls_kwargs:
                 del cls_kwargs[key]
 
