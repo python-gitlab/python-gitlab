5fe7e27bb16a06271f87bf19473b8604df92b4f7
Adam Reid <areid@navtech.aero>
2016-05-06 09:44:15 -0400

    Enable updates on ProjectIssueNotes

--- gitlab/objects.py
+++ gitlab/objects.py
@@ -935,7 +935,7 @@ class ProjectHookManager(BaseManager):
 class ProjectIssueNote(GitlabObject):
     _url = '/projects/%(project_id)s/issues/%(issue_id)s/notes'
     _constructorTypes = {'author': 'User'}
-    canUpdate = False
+    canUpdate = True
     canDelete = False
     requiredUrlAttrs = ['project_id', 'issue_id']
     requiredCreateAttrs = ['body']
