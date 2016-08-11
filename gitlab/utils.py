e0d226bab60bebd3bda28d40d0d13fa282669b9e
Gauvain Pocentek <gauvain@pocentek.net>
2016-08-11 19:57:42 +0200

    fix pep8 test

--- gitlab/utils.py
+++ gitlab/utils.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 class _StdoutStream(object):
     def __call__(self, chunk):
         print(chunk)
