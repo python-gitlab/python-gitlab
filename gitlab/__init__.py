90ad2dec7a0b158b2e77ae0cb403b01f2e1498d4
Gauvain Pocentek <gauvain@pocentek.net>
2016-09-03 20:25:41 +0200

    0.15.1 release

--- gitlab/__init__.py
+++ gitlab/__init__.py
@@ -33,7 +33,7 @@ from gitlab.exceptions import *  # noqa
 from gitlab.objects import *  # noqa
 
 __title__ = 'python-gitlab'
-__version__ = '0.15'
+__version__ = '0.15.1'
 __author__ = 'Gauvain Pocentek'
 __email__ = 'gauvain@pocentek.net'
 __license__ = 'LGPL3'
