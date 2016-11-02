6bedfc32e1f35e21ab3f1c6f0a2cf5c66b06a95e
Asher256@users.noreply.github.com <Asher256>
2016-11-01 20:26:08 -0400

    Project deploy key response code = 201

--- gitlab/objects.py
+++ gitlab/objects.py
@@ -1249,21 +1249,21 @@ class ProjectKey(GitlabObject):
     requiredUrlAttrs = ['project_id']
     requiredCreateAttrs = ['title', 'key']
 
-    def enable(self, key_id):
-        """Enable a deploy key for a project."""
-        url = '/projects/%s/deploy_keys/%s/enable' % (self.project_id, key_id)
-        r = self.gitlab._raw_post(url)
-        raise_error_from_response(r, GitlabProjectDeployKeyError, 200)
-
-    def disable(self, key_id):
-        """Disable a deploy key for a project."""
-        url = '/projects/%s/deploy_keys/%s/disable' % (self.project_id, key_id)
-        r = self.gitlab._raw_delete(url)
-        raise_error_from_response(r, GitlabProjectDeployKeyError, 200)
-
 
 class ProjectKeyManager(BaseManager):
     obj_cls = ProjectKey
+
+    def enable(self, key_id):
+        """Enable a deploy key for a project."""
+        url = '/projects/%s/deploy_keys/%s/enable' % (self.parent.id, key_id)
+        r = self.gitlab._raw_post(url)
+        raise_error_from_response(r, GitlabProjectDeployKeyError, 201)
+
+    def disable(self, key_id):
+        """Disable a deploy key for a project."""
+        url = '/projects/%s/deploy_keys/%s/disable' % (self.parent.id, key_id)
+        r = self.gitlab._raw_delete(url)
+        raise_error_from_response(r, GitlabProjectDeployKeyError, 201)
 
 
 class ProjectEvent(GitlabObject):
