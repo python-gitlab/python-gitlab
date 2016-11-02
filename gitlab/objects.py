cd5f84967444cc07cf9e7bdfd63324ad4890b370
Gauvain Pocentek <gauvain@pocentek.net>
2016-11-02 14:45:37 +0100

    ProjectHook: support the token attribute
    Fix #170

--- gitlab/objects.py
+++ gitlab/objects.py
@@ -1288,7 +1288,7 @@ class ProjectHook(GitlabObject):
     requiredCreateAttrs = ['url']
     optionalCreateAttrs = ['push_events', 'issues_events', 'note_events',
                            'merge_requests_events', 'tag_push_events',
-                           'build_events', 'enable_ssl_verification']
+                           'build_events', 'enable_ssl_verification', 'token']
     shortPrintAttr = 'url'
 
 
