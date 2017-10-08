316754dd8290ee80c8c197eb1eca559fce97792e
Gauvain Pocentek <gauvain@pocentek.net>
2017-10-08 09:55:39 +0200

    Drop leftover pdb call

--- gitlab/mixins.py
+++ gitlab/mixins.py
@@ -235,7 +235,6 @@ class DeleteMixin(object):
             GitlabAuthenticationError: If authentication is not correct
             GitlabDeleteError: If the server cannot perform the request
         """
-        import pdb; pdb.set_trace()
         if not isinstance(id, int):
             id = id.replace('/', '%2F')
         path = '%s/%s' % (self.path, id)
