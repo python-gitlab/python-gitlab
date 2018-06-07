7e89fd08c62fb0c47df7f5c059116e5323c1b20f
Eric Sabouraud <esabouraud@users.noreply.github.com>
2018-06-07 22:17:00 +0200

    Remove unused _create_attrs (object no longer a CRUDMixin)

--- gitlab/v4/objects.py
+++ gitlab/v4/objects.py
@@ -2372,11 +2372,6 @@ class ProjectPushRulesManager(GetWithoutIdMixin, UpdateMixin, RESTManager):
     _path = '/projects/%(project_id)s/push_rule'
     _obj_cls = ProjectPushRules
     _from_parent_attrs = {'project_id': 'id'}
-    _create_attrs = (tuple(),
-                     ('deny_delete_tag', 'member_check',
-                      'prevent_secrets', 'commit_message_regex',
-                      'branch_name_regex', 'author_email_regex',
-                      'file_name_regex', 'max_file_size'))
     _update_attrs = (tuple(),
                      ('deny_delete_tag', 'member_check',
                       'prevent_secrets', 'commit_message_regex',
