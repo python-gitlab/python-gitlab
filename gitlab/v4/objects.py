0304ed363b990ecc84cc6bb47034c4e6b41ced06
Eric Sabouraud <esabouraud@users.noreply.github.com>
2018-06-11 11:04:02 +0200

    Implement create/delete for push_rule endpoint

--- gitlab/v4/objects.py
+++ gitlab/v4/objects.py
@@ -2364,14 +2364,20 @@ class ProjectPipelineScheduleManager(CRUDMixin, RESTManager):
                      ('description', 'ref', 'cron', 'cron_timezone', 'active'))
 
 
-class ProjectPushRules(SaveMixin, RESTObject):
+class ProjectPushRules(SaveMixin, ObjectDeleteMixin, RESTObject):
     _id_attr = None
 
 
-class ProjectPushRulesManager(GetWithoutIdMixin, UpdateMixin, RESTManager):
+class ProjectPushRulesManager(GetWithoutIdMixin, CreateMixin, UpdateMixin,
+                              DeleteMixin, RESTManager):
     _path = '/projects/%(project_id)s/push_rule'
     _obj_cls = ProjectPushRules
     _from_parent_attrs = {'project_id': 'id'}
+    _create_attrs = (tuple(),
+                     ('deny_delete_tag', 'member_check',
+                      'prevent_secrets', 'commit_message_regex',
+                      'branch_name_regex', 'author_email_regex',
+                      'file_name_regex', 'max_file_size'))
     _update_attrs = (tuple(),
                      ('deny_delete_tag', 'member_check',
                       'prevent_secrets', 'commit_message_regex',
