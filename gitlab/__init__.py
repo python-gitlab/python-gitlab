c817dccde8c104dcb294bbf1590c7e3ae9539466
Max Wittig <max.wittig@siemens.com>
2019-12-18 13:57:05 +0100

    chore: bump to 2.0.0
    Dropping support for legacy python requires a new major version

--- gitlab/__init__.py
+++ gitlab/__init__.py
@@ -30,7 +30,7 @@ from gitlab.exceptions import *  # noqa
 from gitlab import utils  # noqa
 
 __title__ = "python-gitlab"
-__version__ = "1.15.0"
+__version__ = "2.0.0"
 __author__ = "Gauvain Pocentek"
 __email__ = "gauvainpocentek@gmail.com"
 __license__ = "LGPL3"
