216ba735e8cfa08695c04491d00c9dd556f1ac5d
Nejc Habjan <hab.nejc@gmail.com>
2020-02-29 13:43:39 +0100

    fix: ensure reset_gitlab works for teardown

--- tools/reset_gitlab.py
+++ tools/reset_gitlab.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 
 import sys
+import time
 
 from gitlab import Gitlab
 
 
 def main():
     with Gitlab.from_config(config_files=["/tmp/python-gitlab.cfg"]) as gl:
-        for project in gl.projects.list():
+        for project in gl.projects.list(all=True):
             project.delete()
-        for group in gl.groups.list():
+        for group in gl.groups.list(all=True):
             group.delete()
+
+        # Gitlab needs time to delete resources; avoid 409 on user deletion
+        while gl.projects.list(all=True) or gl.groups.list(all=True):
+            time.sleep(0.1)
+
         for user in gl.users.list():
             if user.username != "root":
-                user.delete()
+                user.delete(hard_delete=True)
 
 
 if __name__ == "__main__":
