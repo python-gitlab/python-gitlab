6ce5d1f14060a403f05993d77bf37720c25534ba
Nejc Habjan <hab.nejc@gmail.com>
2020-04-05 16:12:11 +0200

    chore(mixins): factor out export download into ExportMixin

--- gitlab/v4/objects.py
+++ gitlab/v4/objects.py
@@ -4097,35 +4097,8 @@ class ProjectWikiManager(CRUDMixin, RESTManager):
     _list_filters = ("with_content",)
 
 
-class ProjectExport(RefreshMixin, RESTObject):
+class ProjectExport(ExportMixin, RefreshMixin, RESTObject):
     _id_attr = None
-
-    @cli.register_custom_action("ProjectExport")
-    @exc.on_http_error(exc.GitlabGetError)
-    def download(self, streamed=False, action=None, chunk_size=1024, **kwargs):
-        """Download the archive of a project export.
-
-        Args:
-            streamed (bool): If True the data will be processed by chunks of
-                `chunk_size` and each chunk is passed to `action` for
-                reatment
-            action (callable): Callable responsible of dealing with chunk of
-                data
-            chunk_size (int): Size of each chunk
-            **kwargs: Extra options to send to the server (e.g. sudo)
-
-        Raises:
-            GitlabAuthenticationError: If authentication is not correct
-            GitlabGetError: If the server failed to perform the request
-
-        Returns:
-            str: The blob content if streamed is False, None otherwise
-        """
-        path = "/projects/%s/export/download" % self.project_id
-        result = self.manager.gitlab.http_get(
-            path, streamed=streamed, raw=True, **kwargs
-        )
-        return utils.response_content(result, streamed, action, chunk_size)
 
 
 class ProjectExportManager(GetWithoutIdMixin, CreateMixin, RESTManager):
