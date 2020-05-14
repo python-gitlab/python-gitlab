b7eab1b232e7209738ee9f0880200d17ceb25e0d
Ludwig Weiss <ludwig.weiss@umusic.com>
2020-05-14 10:35:49 +0200

    feat(api): add deployment mergerequests interface

--- gitlab/v4/objects/deployments.py
+++ gitlab/v4/objects/deployments.py
@@ -1,6 +1,8 @@
 from gitlab.base import RequiredOptional, RESTManager, RESTObject
 from gitlab.mixins import CreateMixin, RetrieveMixin, SaveMixin, UpdateMixin
 
+from .merge_requests import ProjectDeploymentMergeRequestManager  # noqa: F401
+
 __all__ = [
     "ProjectDeployment",
     "ProjectDeploymentManager",
@@ -8,14 +10,21 @@ __all__ = [
 
 
 class ProjectDeployment(SaveMixin, RESTObject):
-    pass
+    _managers = (("mergerequests", "ProjectDeploymentMergeRequestManager"),)
 
 
 class ProjectDeploymentManager(RetrieveMixin, CreateMixin, UpdateMixin, RESTManager):
     _path = "/projects/%(project_id)s/deployments"
     _obj_cls = ProjectDeployment
     _from_parent_attrs = {"project_id": "id"}
-    _list_filters = ("order_by", "sort")
+    _list_filters = (
+        "order_by",
+        "sort",
+        "updated_after",
+        "updated_before",
+        "environment",
+        "status",
+    )
     _create_attrs = RequiredOptional(
         required=("sha", "ref", "tag", "status", "environment")
     )
