323c2799ce0ea7e64f7b54175cbefbfbb9c6c297
Oleksii Shkurupii <Oleksii.Shkurupii@playtech.com>
2020-09-14 15:09:14 +0300

    test: add one more test which lists all members including inherited

--- tools/python_test_v4.py
+++ tools/python_test_v4.py
@@ -312,7 +312,8 @@ assert len(group2.members.list()) == 2
 
 group1.members.delete(user1.id)
 assert len(group1.members.list()) == 2
-assert len(group1.members.all())
+assert len(group1.members.all())  # Deprecated
+assert len(group1.members_all.list())
 member = group1.members.get(user2.id)
 member.access_level = gitlab.const.OWNER_ACCESS
 member.save()
