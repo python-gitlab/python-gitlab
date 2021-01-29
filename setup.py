62dd07df98341f35c8629e8f0a987b35b70f7fe6
Max Wittig <max.wittig@siemens.com>
2021-01-29 09:23:55 +0100

    chore: offically support and test 3.9

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     extras_require={
         "autocompletion": ["argcomplete>=1.10.0,<2"],
