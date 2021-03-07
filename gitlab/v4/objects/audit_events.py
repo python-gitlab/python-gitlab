48ba88ffb983207da398ea2170c867f87a8898e9
Nejc Habjan <hab.nejc@gmail.com>
2021-03-07 12:23:39 +0100

    refactor(objects): move instance audit events where they belong

--- gitlab/v4/objects/audit_events.py
+++ gitlab/v4/objects/audit_events.py
@@ -1,15 +1,27 @@
 """
 GitLab API:
-https://docs.gitlab.com/ee/api/audit_events.html#project-audit-events
+https://docs.gitlab.com/ee/api/audit_events.html
 """
 
 from gitlab.base import RESTManager, RESTObject
 from gitlab.mixins import RetrieveMixin
 
 __all__ = [
+    "AuditEvent",
+    "AuditEventManager",
     "ProjectAudit",
     "ProjectAuditManager",
 ]
+
+
+class AuditEvent(RESTObject):
+    _id_attr = "id"
+
+
+class AuditEventManager(ListMixin, RESTManager):
+    _path = "/audit_events"
+    _obj_cls = AuditEvent
+    _list_filters = ("created_after", "created_before", "entity_type", "entity_id")
 
 
 class ProjectAudit(RESTObject):
