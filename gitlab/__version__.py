34c4052327018279c9a75d6b849da74eccc8819b
Max Wittig <max.wittig@siemens.com>
2021-04-23 16:40:00 +0200

    chore: bump version to 2.7.0

--- gitlab/__version__.py
+++ gitlab/__version__.py
@@ -3,4 +3,4 @@ __copyright__ = "Copyright 2013-2019 Gauvain Pocentek, 2019-2021 python-gitlab t
 __email__ = "gauvainpocentek@gmail.com"
 __license__ = "LGPL3"
 __title__ = "python-gitlab"
-__version__ = "2.6.0"
+__version__ = "2.7.0"
