1099e96bdb7ea2f8643bcff69b8ecdfc6213a2d0
Antti Tanhuanpää <antti.tanhuanpaa@anders.fi>
2021-06-29 13:15:40 +0300

    fix(api): replace deprecated attribute in delete_in_bulk()
    The `name_regex` attribute has been deprecated in favor of
    `name_regex_delete`.
    See: https://gitlab.com/gitlab-org/gitlab/-/commit/ce99813cf54

--- gitlab/v4/objects/container_registry.py
+++ gitlab/v4/objects/container_registry.py
@@ -31,26 +31,28 @@ class ProjectRegistryTagManager(DeleteMixin, RetrieveMixin, RESTManager):
     _path = "/projects/%(project_id)s/registry/repositories/%(repository_id)s/tags"
 
     @cli.register_custom_action(
-        "ProjectRegistryTagManager", optional=("name_regex", "keep_n", "older_than")
+        "ProjectRegistryTagManager",
+        ("name_regex_delete",),
+        optional=("keep_n", "name_regex_keep", "older_than"),
     )
     @exc.on_http_error(exc.GitlabDeleteError)
-    def delete_in_bulk(self, name_regex=".*", **kwargs):
+    def delete_in_bulk(self, name_regex_delete, **kwargs):
         """Delete Tag in bulk
 
         Args:
-            name_regex (string):      The regex of the name to delete. To delete all
-                                      tags specify .*.
-            keep_n (integer):         The amount of latest tags of given name to keep.
-            name_regex_keep (string): The regex of the name to keep. This value
-                                      overrides any matches from name_regex.
-            older_than (string):      Tags to delete that are older than the given time,
-                                      written in human readable form 1h, 1d, 1month.
-            **kwargs:                 Extra options to send to the server (e.g. sudo)
+            name_regex_delete (string): The regex of the name to delete. To delete all
+                                        tags specify .*.
+            keep_n (integer):           The amount of latest tags of given name to keep.
+            name_regex_keep (string):   The regex of the name to keep. This value
+                                        overrides any matches from name_regex.
+            older_than (string):        Tags to delete that are older than the given time,
+                                        written in human readable form 1h, 1d, 1month.
+            **kwargs:                   Extra options to send to the server (e.g. sudo)
         Raises:
             GitlabAuthenticationError: If authentication is not correct
             GitlabDeleteError: If the server cannot perform the request
         """
         valid_attrs = ["keep_n", "name_regex_keep", "older_than"]
-        data = {"name_regex": name_regex}
+        data = {"name_regex_delete": name_regex_delete}
         data.update({k: v for k, v in kwargs.items() if k in valid_attrs})
         self.gitlab.http_delete(self.path, query_data=data, **kwargs)
