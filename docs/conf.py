a349793307e3a975bb51f864b48e5e9825f70182
Nejc Habjan <hab.nejc@gmail.com>
2022-01-06 00:03:29 +0100

    chore: add temporary banner for v3

--- docs/conf.py
+++ docs/conf.py
@@ -121,7 +121,10 @@ html_theme = "furo"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    "announcement": "⚠ python-gitlab 3.0.0 has been released with several "
+    "<a href='changelog.html#breaking'>breaking changes</a>.",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
