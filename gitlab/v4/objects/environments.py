6f64d4098ed4a890838c6cf43d7a679e6be4ac6c
Nejc Habjan <nejc.habjan@siemens.com>
2022-01-13 18:04:11 +0100

    fix(cli): add missing list filters for environments

--- gitlab/v4/objects/environments.py
+++ gitlab/v4/objects/environments.py
@@ -48,6 +48,7 @@ class ProjectEnvironmentManager(
     _from_parent_attrs = {"project_id": "id"}
     _create_attrs = RequiredOptional(required=("name",), optional=("external_url",))
     _update_attrs = RequiredOptional(optional=("name", "external_url"))
+    _list_filters = ("name", "search", "states")
 
     def get(
         self, id: Union[str, int], lazy: bool = False, **kwargs: Any
