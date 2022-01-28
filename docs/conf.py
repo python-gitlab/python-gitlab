c0ade7588be063b64e5006f70334a86a5ace991e
Wadim Klincov <wadim.klincov@siemens.com>
2022-01-28 18:53:44 +0100

    Revert "chore: add temporary banner for v3"
    This reverts commit a349793307e3a975bb51f864b48e5e9825f70182.

--- docs/conf.py
+++ docs/conf.py
@@ -121,10 +121,7 @@ html_theme = "furo"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "announcement": "⚠ python-gitlab 3.0.0 has been released with several "
-    "<a href='changelog.html#breaking'>breaking changes</a>.",
-}
+# html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
