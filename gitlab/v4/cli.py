dc08093b78a457bf5507e215c620f8a972b355c2
Waldek Maleska <w.maleska@gmail.com>
2022-07-04 09:31:56 +0100

    feat(cli): add a custom help formatter
    Add a custom argparse help formatter that overrides the output
    format to list items vertically.
    
    The formatter is derived from argparse.HelpFormatter with minimal changes.
    
    Co-authored-by: John Villalovos <john@sodarock.com>
    Co-authored-by: Nejc Habjan <nejc.habjan@siemens.com>

--- gitlab/v4/cli.py
+++ gitlab/v4/cli.py
@@ -377,7 +377,9 @@ def extend_parser(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
 
     for cls in sorted(classes, key=operator.attrgetter("__name__")):
         arg_name = cli.cls_to_gitlab_resource(cls)
-        object_group = subparsers.add_parser(arg_name)
+        object_group = subparsers.add_parser(
+            arg_name, formatter_class=cli.VerticalHelpFormatter
+        )
 
         object_subparsers = object_group.add_subparsers(
             title="action",
