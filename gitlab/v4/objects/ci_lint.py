47bfd769e9cbe9ee71f5dba36089aae0ca6eda8d
Nejc Habjan <nejc.habjan@siemens.com>
2022-07-05 11:51:56 +0200

    chore(ci_lint): add create attributes

--- gitlab/v4/objects/ci_lint.py
+++ gitlab/v4/objects/ci_lint.py
@@ -7,6 +7,7 @@ from typing import Any, cast
 
 from gitlab.base import RESTManager, RESTObject
 from gitlab.mixins import CreateMixin, GetWithoutIdMixin
+from gitlab.types import RequiredOptional
 
 
 class ProjectCiLint(RESTObject):
@@ -17,6 +18,9 @@ class ProjectCiLintManager(GetWithoutIdMixin, CreateMixin, RESTManager):
     _path = "/projects/{project_id}/ci/lint"
     _obj_cls = ProjectCiLint
     _from_parent_attrs = {"project_id": "id"}
+    _create_attrs = RequiredOptional(
+        required=("content",), optional=("dry_run", "include_jobs", "ref")
+    )
 
     def get(self, **kwargs: Any) -> ProjectCiLint:
         return cast(ProjectCiLint, super().get(**kwargs))
