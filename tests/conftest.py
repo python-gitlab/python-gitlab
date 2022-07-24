8d4f13b192afd5d4610eeaf2bbea71c3b6a25964
Nejc Habjan <nejc.habjan@siemens.com>
2022-07-24 22:53:01 +0200

    test: always ensure clean config environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,27 @@
 import pytest
 
+import gitlab
+
 
 @pytest.fixture(scope="session")
 def test_dir(pytestconfig):
     return pytestconfig.rootdir / "tests"
+
+
+@pytest.fixture(autouse=True)
+def mock_clean_config(monkeypatch):
+    """Ensures user-defined environment variables do not interfere with tests."""
+    monkeypatch.delenv("PYTHON_GITLAB_CFG", raising=False)
+    monkeypatch.delenv("GITLAB_PRIVATE_TOKEN", raising=False)
+    monkeypatch.delenv("GITLAB_URL", raising=False)
+    monkeypatch.delenv("CI_JOB_TOKEN", raising=False)
+    monkeypatch.delenv("CI_SERVER_URL", raising=False)
+
+
+@pytest.fixture(autouse=True)
+def default_files(monkeypatch):
+    """Ensures user configuration files do not interfere with tests."""
+    monkeypatch.setattr(gitlab.config, "_DEFAULT_FILES", [])
 
 
 @pytest.fixture
