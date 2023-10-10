71fca8c8f5c7f3d6ab06dd4e6c0d91003705be09
Nejc Habjan <nejc.habjan@siemens.com>
2023-10-10 22:28:29 +0200

    refactor(build): build project using PEP 621
    BREAKING CHANGE: python-gitlab now stores metadata in pyproject.toml
    as per PEP 621, with setup.py removed. pip version v21.1 or higher is
    required if you want to perform an editable install.

--- tests/smoke/test_dists.py
+++ tests/smoke/test_dists.py
@@ -1,34 +1,40 @@
 import tarfile
 import zipfile
+import subprocess
+import sys
 from pathlib import Path
-from sys import version_info
+
 
 import pytest
-from setuptools import sandbox
 
 from gitlab._version import __title__, __version__
 
-DIST_DIR = Path("dist")
 DOCS_DIR = "docs"
 TEST_DIR = "tests"
 SDIST_FILE = f"{__title__}-{__version__}.tar.gz"
-WHEEL_FILE = (
-    f"{__title__.replace('-', '_')}-{__version__}-py{version_info.major}-none-any.whl"
-)
+WHEEL_FILE = f"{__title__.replace('-', '_')}-{__version__}-py{sys.version_info.major}-none-any.whl"
 
 
-@pytest.fixture(scope="function")
-def build() -> None:
-    sandbox.run_setup("setup.py", ["--quiet", "clean", "--all"])  # type: ignore[no-untyped-call]
-    sandbox.run_setup("setup.py", ["--quiet", "sdist", "bdist_wheel"])  # type: ignore[no-untyped-call]
+@pytest.fixture(scope="session")
+def build(tmp_path_factory: Path):
+    temp_dir = tmp_path_factory.mktemp("build")
+    subprocess.run([sys.executable, "-m", "build", "--outdir", temp_dir], check=True)
+    return temp_dir
 
 
-def test_sdist_includes_tests(build):
-    sdist = tarfile.open(DIST_DIR / SDIST_FILE, "r:gz")
-    test_dir = sdist.getmember(f"{__title__}-{__version__}/{TEST_DIR}")
+def test_sdist_includes_docs_and_tests(build: subprocess.CompletedProcess) -> None:
+    sdist = tarfile.open(build / SDIST_FILE, "r:gz")
+    sdist_dir = f"{__title__}-{__version__}"
+
+    docs_dir = sdist.getmember(f"{sdist_dir}/{DOCS_DIR}")
+    test_dir = sdist.getmember(f"{sdist_dir}/{TEST_DIR}")
+    readme = sdist.getmember(f"{sdist_dir}/README.rst")
+
+    assert docs_dir.isdir()
     assert test_dir.isdir()
+    assert readme.isfile()
 
 
-def test_wheel_excludes_docs_and_tests(build):
-    wheel = zipfile.ZipFile(DIST_DIR / WHEEL_FILE)
+def test_wheel_excludes_docs_and_tests(build: subprocess.CompletedProcess) -> None:
+    wheel = zipfile.ZipFile(build / WHEEL_FILE)
     assert not any(file.startswith((DOCS_DIR, TEST_DIR)) for file in wheel.namelist())
