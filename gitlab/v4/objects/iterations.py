a527dbecc6d606566f0b908b6277774af2e9b24b
Cristiano Casella <cristiano.casella@seacom.it>
2024-02-12 09:25:00 +0100

    feat: Added "in" parameter to project/group iteration search

--- gitlab/v4/objects/iterations.py
+++ gitlab/v4/objects/iterations.py
@@ -16,11 +16,11 @@ class GroupIterationManager(ListMixin, RESTManager):
     _path = "/groups/{group_id}/iterations"
     _obj_cls = GroupIteration
     _from_parent_attrs = {"group_id": "id"}
-    _list_filters = ("state", "search", "include_ancestors")
+    _list_filters = ("state", "search", "in", "include_ancestors")
 
 
 class ProjectIterationManager(ListMixin, RESTManager):
     _path = "/projects/{project_id}/iterations"
     _obj_cls = GroupIteration
     _from_parent_attrs = {"project_id": "id"}
-    _list_filters = ("state", "search", "include_ancestors")
+    _list_filters = ("state", "search", "in", "include_ancestors")
