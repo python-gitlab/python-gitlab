9a7f3e29f0825ff59102a30739af6f879662e8a9
Cristiano Casella <cristiano.casella@seacom.it>
2024-02-12 09:25:00 +0100

    feat: Added additional parameter to project/group iteration search

--- gitlab/v4/objects/iterations.py
+++ gitlab/v4/objects/iterations.py
@@ -1,3 +1,4 @@
+from gitlab import types
 from gitlab.base import RESTManager, RESTObject
 from gitlab.mixins import ListMixin
 
@@ -16,11 +17,37 @@ class GroupIterationManager(ListMixin, RESTManager):
     _path = "/groups/{group_id}/iterations"
     _obj_cls = GroupIteration
     _from_parent_attrs = {"group_id": "id"}
-    _list_filters = ("state", "search", "in", "include_ancestors")
+    # When using the API, the "in" keyword collides with python's "in" keyword
+    # raising a SyntaxError.
+    # For this reason, we have to use the query_parameters argument:
+    # group.iterations.list(query_parameters={"in": "title"})
+    _list_filters = (
+        "include_ancestors",
+        "include_descendants",
+        "in",
+        "search",
+        "state",
+        "updated_after",
+        "updated_before",
+    )
+    _types = {"scopes": types.ArrayAttribute}
 
 
 class ProjectIterationManager(ListMixin, RESTManager):
     _path = "/projects/{project_id}/iterations"
     _obj_cls = GroupIteration
     _from_parent_attrs = {"project_id": "id"}
-    _list_filters = ("state", "search", "in", "include_ancestors")
+    # When using the API, the "in" keyword collides with python's "in" keyword
+    # raising a SyntaxError.
+    # For this reason, we have to use the query_parameters argument:
+    # project.iterations.list(query_parameters={"in": "title"})
+    _list_filters = (
+        "include_ancestors",
+        "include_descendants",
+        "in",
+        "search",
+        "state",
+        "updated_after",
+        "updated_before",
+    )
+    _types = {"in": types.ArrayAttribute}
