4203bec5d9fdecd3079016006721983a18561d58
Tim Knight <tim.knight1@engineering.digital.dwp.gov.uk>
2024-02-13 16:15:25 +0000

    test: add api integration test for ci_cd_token allowlist
    Signed-off-by: Tim Knight <tim.knight1@engineering.digital.dwp.gov.uk>

---
+++ tests/functional/api/test_project_job_token_scope.py
@@ -0,0 +1,43 @@
+def test_add_project_to_job_token_scope_allowlist(gl, project):
+    project_to_add = gl.projects.create({"name": "Ci_Cd_token_add_proj"})
+
+    scope = project.job_token_scope.get()
+    resp = scope.allowlist.create({"target_project_id": project_to_add.id})
+
+    assert resp.source_project_id == project.id
+    assert resp.target_project_id == project_to_add.id
+
+    project_to_add.delete()
+
+
+def test_projects_job_token_scope_allowlist_contains_added_project_name(gl, project):
+    scope = project.job_token_scope.get()
+    assert len(scope.allowlist.list()) == 0
+
+    project_name = "Ci_Cd_token_named_proj"
+    project_to_add = gl.projects.create({"name": project_name})
+    scope.allowlist.create({"target_project_id": project_to_add.id})
+
+    scope.refresh()
+    assert any(allowed.name == project_name for allowed in scope.allowlist.list())
+
+    project_to_add.delete()
+
+
+def test_remove_project_by_id_from_projects_job_token_scope_allowlist(gl, project):
+    scope = project.job_token_scope.get()
+    assert len(scope.allowlist.list()) == 0
+
+    project_to_add = gl.projects.create({"name": "Ci_Cd_token_remove_proj"})
+
+    scope.allowlist.create({"target_project_id": project_to_add.id})
+
+    scope.refresh()
+    assert len(scope.allowlist.list()) != 0
+
+    scope.allowlist.remove(project_to_add.id)
+
+    scope.refresh()
+    assert len(scope.allowlist.list()) == 0
+
+    project_to_add.delete()
