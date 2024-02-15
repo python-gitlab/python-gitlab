761ff6741761bea1e7ed092df79f3a57dbaec9b8
Isaac Philip <4974658+isaac-philip@users.noreply.github.com>
2024-02-15 15:47:19 +0530

    feat(functional): merge train -> merge request :> status fetch

--- gitlab/v4/objects/merge_trains.py
+++ gitlab/v4/objects/merge_trains.py
@@ -1,14 +1,32 @@
-from gitlab.base import RESTObject
-from gitlab.mixins import ListMixin
+from gitlab.base import RESTManager, RESTObject
+from gitlab.mixins import CRUDMixin, GetMixin, ListMixin
+from gitlab.types import RequiredOptional
 
-__all__ = ["ProjectMergeTrain", "ProjectMergeTrainManager"]
+__all__ = [
+    "ProjectMergeTrain",
+    "ProjectMergeTrainManager",
+    "ProjectMergeTrainMergeRequest",
+    "ProjectMergeTrainMergeRequestManager",
+]
 
 
-class ProjectMergeTrain(RESTObject):
+class ProjectMergeTrainMergeRequest(RESTObject):
     pass
 
 
-class ProjectMergeTrainManager(ListMixin[ProjectMergeTrain]):
+class ProjectMergeTrainMergeRequestManager(CRUDMixin, RESTManager):
+    _path = "/projects/{project_id}/merge_trains/merge_requests"
+    _obj_cls = ProjectMergeTrainMergeRequest
+    _from_parent_attrs = {"project_id": "project_id"}
+
+    _update_attrs = RequiredOptional(optional=("position",))
+
+
+class ProjectMergeTrain(RESTObject):
+    merge_requests: ProjectMergeTrainMergeRequestManager
+
+
+class ProjectMergeTrainManager(GetMixin, ListMixin, RESTManager):
     _path = "/projects/{project_id}/merge_trains"
     _obj_cls = ProjectMergeTrain
     _from_parent_attrs = {"project_id": "id"}
