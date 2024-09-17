19ffb32dd9d08c51c55f893d56c8fd7107967d37
Mateusz Wozny <mateusz15wozny@gmail.com>
2024-09-17 12:40:14 +0200

    feat(api): refactor branch creation to include default flag black tests

--- tests/functional/api/test_branches.py
+++ tests/functional/api/test_branches.py
@@ -7,10 +7,12 @@ https://docs.gitlab.com/ee/api/branches.html
 def test_branch_name_with_period(project):
     # Make sure we can create and get a branch name containing a period '.'
     branch_name = "my.branch.name"
-    branch = project.branches.create({"branch": branch_name, "ref": "main", "default": True})
+    branch = project.branches.create(
+        {"branch": branch_name, "ref": "main", "default": True}
+    )
     assert branch.name == branch_name
     assert branch.default is True
-    
+
     # Ensure we can get the branch
     fetched_branch = project.branches.get(branch_name)
     assert branch.name == fetched_branch.name
