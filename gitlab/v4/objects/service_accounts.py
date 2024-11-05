499243b37cda0c7dcd4b6ce046d42e81845e2a4f
ka28kumar <ka28kumar@gmail.com>
2024-11-05 22:20:29 +0530

    feat(api): support list and delete for group service accounts (#2963)

--- gitlab/v4/objects/service_accounts.py
+++ gitlab/v4/objects/service_accounts.py
@@ -1,15 +1,15 @@
 from gitlab.base import RESTManager, RESTObject
-from gitlab.mixins import CreateMixin
+from gitlab.mixins import CreateMixin, DeleteMixin, ListMixin, ObjectDeleteMixin
 from gitlab.types import RequiredOptional
 
 __all__ = ["GroupServiceAccount", "GroupServiceAccountManager"]
 
 
-class GroupServiceAccount(RESTObject):
+class GroupServiceAccount(ObjectDeleteMixin, RESTObject):
     pass
 
 
-class GroupServiceAccountManager(CreateMixin, RESTManager):
+class GroupServiceAccountManager(CreateMixin, DeleteMixin, ListMixin, RESTManager):
     _path = "/groups/{group_id}/service_accounts"
     _obj_cls = GroupServiceAccount
     _from_parent_attrs = {"group_id": "id"}
