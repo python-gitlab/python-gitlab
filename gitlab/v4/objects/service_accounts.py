89886c8a67e3624697e9c141f75a32ba7d84fb5d
Rolf Offermanns <rof@sysgo.com>
2025-01-29 09:54:17 +0100

    feat(api): Add support for group service account access tokens.
    Fixes #2946

--- gitlab/v4/objects/service_accounts.py
+++ gitlab/v4/objects/service_accounts.py
@@ -1,12 +1,21 @@
 from gitlab.base import RESTObject
-from gitlab.mixins import CreateMixin, DeleteMixin, ListMixin, ObjectDeleteMixin
-from gitlab.types import RequiredOptional
+from gitlab.mixins import (
+    CreateMixin,
+    DeleteMixin,
+    ListMixin,
+    ObjectDeleteMixin,
+    ObjectRotateMixin,
+    RotateMixin,
+)
+from gitlab.types import ArrayAttribute, RequiredOptional
 
 __all__ = [
     "ServiceAccount",
     "ServiceAccountManager",
     "GroupServiceAccount",
     "GroupServiceAccountManager",
+    "GroupServiceAccountAccessToken",
+    "GroupServiceAccountAccessTokenManager",
 ]
 
 
@@ -14,12 +23,10 @@ class ServiceAccount(RESTObject):
     pass
 
 
-class ServiceAccountManager(CreateMixin, ListMixin, RESTManager):
+class ServiceAccountManager(CreateMixin[ServiceAccount], ListMixin[ServiceAccount]):
     _path = "/service_accounts"
     _obj_cls = ServiceAccount
-    _create_attrs = RequiredOptional(
-        optional=("name", "username", "email"),
-    )
+    _create_attrs = RequiredOptional(optional=("name", "username", "email"))
 
 
 class GroupServiceAccount(ObjectDeleteMixin, RESTObject):
@@ -35,3 +42,20 @@ class GroupServiceAccountManager(
     _obj_cls = GroupServiceAccount
     _from_parent_attrs = {"group_id": "id"}
     _create_attrs = RequiredOptional(optional=("name", "username"))
+
+
+class GroupServiceAccountAccessToken(ObjectRotateMixin, RESTObject):
+    pass
+
+
+class GroupServiceAccountAccessTokenManager(
+    CreateMixin[GroupServiceAccountAccessToken],
+    RotateMixin[GroupServiceAccountAccessToken],
+):
+    _path = "/groups/{group_id}/service_accounts/{user_id}/personal_access_tokens"
+    _obj_cls = GroupServiceAccountAccessToken
+    _from_parent_attrs = {"group_id": "id", "user_id": "user_id"}
+    _create_attrs = RequiredOptional(
+        required=("name", "scopes"), optional=("expires_at",)
+    )
+    _types = {"scopes": ArrayAttribute}
