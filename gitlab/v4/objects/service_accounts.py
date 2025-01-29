a0777d455a9274e5d5051339b4c9721626dae450
Rolf Offermanns <rof@sysgo.com>
2025-01-29 09:46:36 +0100

    feat(api): Add instance level service accounts (EE)
    Fixes #2812.

--- gitlab/v4/objects/service_accounts.py
+++ gitlab/v4/objects/service_accounts.py
@@ -2,7 +2,24 @@ from gitlab.base import RESTObject
 from gitlab.mixins import CreateMixin, DeleteMixin, ListMixin, ObjectDeleteMixin
 from gitlab.types import RequiredOptional
 
-__all__ = ["GroupServiceAccount", "GroupServiceAccountManager"]
+__all__ = [
+    "ServiceAccount",
+    "ServiceAccountManager",
+    "GroupServiceAccount",
+    "GroupServiceAccountManager",
+]
+
+
+class ServiceAccount(RESTObject):
+    pass
+
+
+class ServiceAccountManager(CreateMixin, ListMixin, RESTManager):
+    _path = "/service_accounts"
+    _obj_cls = ServiceAccount
+    _create_attrs = RequiredOptional(
+        optional=("name", "username", "email"),
+    )
 
 
 class GroupServiceAccount(ObjectDeleteMixin, RESTObject):
