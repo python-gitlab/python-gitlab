6a82e82aa402b0e579b1e50e3d3762167700edea
malmor <62105800+malmor@users.noreply.github.com>
2026-01-28 13:06:43 +0100

    feat(api): add support for sort filter in runner jobs
    This commits allows users to configure the 'sort' filter as
    defined in [1], so that one can fetch the most recent jobs
    first.
    
    [1]: https://docs.gitlab.com/api/runners/#list-all-jobs-processed-by-a-runner

--- gitlab/v4/objects/runners.py
+++ gitlab/v4/objects/runners.py
@@ -38,7 +38,7 @@ class RunnerJobManager(ListMixin[RunnerJob]):
     _path = "/runners/{runner_id}/jobs"
     _obj_cls = RunnerJob
     _from_parent_attrs = {"runner_id": "id"}
-    _list_filters = ("status",)
+    _list_filters = ("status", "sort")
 
 
 class Runner(SaveMixin, ObjectDeleteMixin, RESTObject):
