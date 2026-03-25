188c96283dc0b49ca354952c5ea2f4fc519d28cc
Philip Nelson <philipnelson5@gmail.com>
2026-03-25 16:06:18 -0600

    test: add test for hidden variables

--- tests/functional/api/test_variables.py
+++ tests/functional/api/test_variables.py
@@ -43,3 +43,51 @@ def test_project_variables(project):
     assert variable.value == "new_value1"
 
     variable.delete()
+
+
+def test_hidden_instance_variables(gl):
+    variable = gl.variables.create(
+        {"key": "key1", "value": "secret_value", "masked_and_hidden": True}
+    )
+    assert variable.value == "secret_value"
+    assert variable.description is None
+    assert variable in gl.variables.list()
+
+    variable.description = "new_description"
+    variable.save()
+    variable = gl.variables.get(variable.key)
+    assert variable.description == "new_description"
+
+    variable.delete()
+
+
+def test_hidden_group_variables(group):
+    variable = group.variables.create(
+        {"key": "key1", "value": "secret_value", "masked_and_hidden": True}
+    )
+    assert variable.value is None
+    assert variable.description is None
+    assert variable in group.variables.list()
+
+    variable.description = "new_description"
+    variable.save()
+    variable = group.variables.get(variable.key)
+    assert variable.description == "new_description"
+
+    variable.delete()
+
+
+def test_hidden_project_variables(project):
+    variable = project.variables.create(
+        {"key": "key1", "value": "secret_value", "masked_and_hidden": True}
+    )
+    assert variable.value is None
+    assert variable.description is None
+    assert variable in project.variables.list()
+
+    variable.description = "new_description"
+    variable.save()
+    variable = project.variables.get(variable.key)
+    assert variable.description == "new_description"
+
+    variable.delete()
